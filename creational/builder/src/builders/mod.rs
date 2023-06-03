be9445210d275cb8de0849dc68badd2743434c80
jinghua@nyquistdata.com <jinghua@nyquistdata.com>
2023-06-03 15:31:17 +0800

    feat: builder pattern

---
+++ creational/builder/src/builders/mod.rs
@@ -0,0 +1,18 @@
+use crate::builders::car::DEFAULT_FUEL;
+use crate::components::{CarType, Engine, GpsNavigator, Transmission};
+
+mod car;
+mod car_manual;
+
+pub trait Builder {
+    type OutputType;
+    fn set_car_type(&mut self, car_type: CarType);
+    fn set_seats(&mut self, seats: u16);
+    fn set_engine(&mut self, engine: Engine);
+    fn set_transmission(&mut self, transmission: Transmission);
+    fn set_gsp_navigator(&mut self, gps_navigator: GpsNavigator);
+    fn build(self) -> Self::OutputType;
+}
+
+pub use car::CarBuilder;
+pub use car_manual::CarManualBuilder;
